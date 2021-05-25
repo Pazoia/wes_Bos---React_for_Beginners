afdc1aa604ed94ebb3f952e1dd35d97b5557eea4
Paulo Azoia <pazoia.gomes@gmail.com>
2021-05-25 18:32:14 +0100

    Started working with JSX:
    Created the form store-selector
    added h2, input and button elements

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,8 +2,34 @@ import React from "react";
 
 class StorePicker extends React.Component {
     render() {
-        return <p>I am the Store Picker!</p>;
+        return (
+            <form className="store">
+                <h2>Please Enter a Name</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store →</button>
+            </form>
+        );
     }
 }
 
 export default StorePicker;
+
+/*
+    In order to add sibling elements to be rendered
+    in a render function, add a set of empty angle brackets
+    and put all the elements inside them.
+
+    Example:
+    return (
+        <>
+            <p>This is to show how to add multiple sibling</p>
+            <p>elements to a render method</p>
+            <h2 className="name">My name is Paulo</h2>
+            <form className="store">
+                <h2>Please Enter a Name</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store →</button>
+            </form>
+        </>
+        );
+*/
